dc796387f3216d6804848659e5de1f846ac881f5
hyejin-1010 <hyepago@gmail.com>
2023-05-29 18:18:26 +0900

    Mod: Vote

--- src/utills/checkVoted.ts
+++ src/utills/checkVoted.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export default function checkVoted() {
-  return axios.post('https://www.moon-rises-in-space.store/CheckVoted'); 
+  return axios.post('https://www.moon-rises-in-space.store/CheckVoted', {}, {
+    withCredentials: true,
+  }); 
 }
