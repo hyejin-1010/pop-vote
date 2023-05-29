8dcb4861396f8d3f7553b756cc991b9805ecea29
hyejin-1010 <hyepago@gmail.com>
2023-05-29 19:18:18 +0900

    Add: proxy 설정

--- src/utills/checkVoted.ts
+++ src/utills/checkVoted.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default function checkVoted() {
-  return axios.post('http://internal-ilb-1826088408.ap-northeast-2.elb.amazonaws.com/CheckVoted', {}, {
+  return axios.post('/CheckVoted', {}, {
     withCredentials: true,
   }); 
 }
--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,12 @@ export default defineConfig({
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
   },
+  devServer: {
+    proxy: {
+      '/CheckVoted': { 
+        target: 'http://internal-ilb-1826088408.ap-northeast-2.elb.amazonaws.com',
+        changeOrigin: true,
+      }, 
+    },
+  },
 });
